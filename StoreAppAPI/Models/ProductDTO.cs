8fc75d6efd27d7ee66984585f7f8fdbcaf12ea83
Patryk <patryk.bogdanski95@gmail.com>
2022-09-07 11:10:44 +0200

    Add Id to ProductDTO

--- StoreAppAPI/Models/ProductDTO.cs
+++ StoreAppAPI/Models/ProductDTO.cs
@@ -9,6 +9,7 @@ namespace StoreAppAPI.Models
 {
     public class ProductDTO
     {
+        public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
         public decimal Price { get; set; }
